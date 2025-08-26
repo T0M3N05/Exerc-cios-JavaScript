a1a961ad96f7f1e8369af2be42405a564c79d25f
Gabriel Emanuel <88034935+T0M3N05@users.noreply.github.com>
2025-08-26 20:36:28 -0300

    Atividade4Ex6.java

--- Atividade4Ex6.java
+++ Atividade4Ex6.java
@@ -5,11 +5,19 @@ public class Atividade4Ex6 {
 
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        
+
+        String nomeAluno;
+        int idade;
         int nota1;
         int nota2;
         int nota3;
         int nota4;
+
+        System.out.println("Insira o nome do aluno: ");
+        nomeAluno = ler.nextLine();
+
+        System.out.println("Insira a idade do aluno: ");
+        idade = ler.nextInt();
         
         System.out.println("Insira a primeira nota: ");
         nota1 = ler.nextInt();
@@ -22,6 +30,9 @@ public class Atividade4Ex6 {
         
         System.out.println("Insira a quarta nota: ");
         nota4 = ler.nextInt();
+
+        System.out.println(nomeAluno);
+        System.out.printIn(idade);
         
         System.out.println("A média das notas é: ");
         System.out.println((nota1 + nota2 + nota3 + nota4) / 4);
